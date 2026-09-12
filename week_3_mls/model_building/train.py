97ed1470646598fb6dee4cbdd09b60ce83310a25
piyali1988 <piyali1988@gmail.com>
2026-09-12 13:07:29 +0100

    update week_3_mls/model_building/train.py

--- week_3_mls/model_building/train.py
+++ week_3_mls/model_building/train.py
@@ -52,7 +52,7 @@ model_pipeline = make_pipeline(preprocessor, xgb_model)
 # Start MLflow run
 with mlflow.start_run():
     # Hyperparameter tuning
-    grid_search = GridSearchCV(model_pipeline, param_grid, cv=5, scoring="recall", n_jobs=-1)
+    grid_search = GridSearchCV(model_pipeline, param_grid, cv=3, scoring="recall", n_jobs=-1)
     grid_search.fit(Xtrain, ytrain)
 
     # Log all parameter combinations and their mean test scores
